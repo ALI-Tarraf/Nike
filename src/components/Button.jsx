326eabaa400d81d7ea179bc439c65b118ff1b060
ALI-Tarraf <alitarraf707@gmail.com>
2024-05-20 14:42:55 +0300

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,32 @@
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  borderColor,
+  textColor,
+  fullwidth,
+}) => {
+  return (
+    <button
+      className={`flex justify-center items-center gap-2 py-4 px-7
+       border font-montserrat test-lg leading-none
+       ${
+         backgroundColor
+           ? `${backgroundColor} ${borderColor} ${textColor} `
+           : "bg-coral-red  text-white border-coral-red"
+       }
+       rounded-full ${fullwidth && "w-full"} `}
+    >
+      {label}
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
+    </button>
+  );
+};
+
+export default Button;
